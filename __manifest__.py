565dd8671e886dd6a75faec80716a6411c0c7e07
Butirpadi <butirpadi@gmail.com>
2018-03-25 22:14:24 +0700

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Keuangan Siswa",
+
+    'summary': """
+        Module Keuangan aplikasi database siswa (siswa_ocb11)""",
+
+    'description': """
+        Module Keuangan aplikasi database siswa (siswa_ocb11)
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','siswa_ocb11','sale_management'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
