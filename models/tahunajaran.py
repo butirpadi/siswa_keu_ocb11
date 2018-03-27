f1600950244e44885b7295d37538cf0b7f8c3b93
Butirpadi <butirpadi@gmail.com>
2018-03-27 12:39:21 +0700

    Update tahunajaran, tahunajaran_jenjang, assign biaya

--- models/tahunajaran.py
+++ models/tahunajaran.py
@@ -6,21 +6,27 @@ class tahunajaran(models.Model):
 	_inherit = 'siswa_ocb11.tahunajaran'
 	
 	# biayas = fields.One2many('siswa_keu_ocb11.tahunajaran_biaya', inverse_name='tahunajaran_id' , string='Biaya-biaya')
-	jenjangs = fields.One2many('siswa_keu_ocb11.tahunajaran_jenjang', inverse_name='tahunajaran_id' , string='Jenjang')
+	jenjangs = fields.One2many('siswa_keu_ocb11.tahunajaran_jenjang', inverse_name='tahunajaran_id' , string='Jenjang', ondelete='cascade')
 	
 	@api.model
 	def create(self, vals):
 		result = super(tahunajaran, self).create(vals)
 	# 	# auto generate tahunajaran_jenjang
+		print(str(result.name) + ' PG')
 		self.env['siswa_keu_ocb11.tahunajaran_jenjang'].create({
+			'name' : str(result.name) + ' PG',
 			'tahunajaran_id' : result.id,
 			'jenjang' : 0
 		})
+		print(str(result.name) + ' TK A')
 		self.env['siswa_keu_ocb11.tahunajaran_jenjang'].create({
+			'name' : str(result.name) + ' TK A',
 			'tahunajaran_id' : result.id,
 			'jenjang' : 1
 		})
+		print(str(result.name) + ' TK B')
 		self.env['siswa_keu_ocb11.tahunajaran_jenjang'].create({
+			'name' : str(result.name) + ' TK B',
 			'tahunajaran_id' : result.id,
 			'jenjang' : 2
 		})
--- models/tahunajaran_jenjang.py
+++ models/tahunajaran_jenjang.py
@@ -6,13 +6,13 @@ class tahunajaran_jenjang(models.Model):
     _name = 'siswa_keu_ocb11.tahunajaran_jenjang'
 
     name = fields.Char(string='Nama')
-    tahunajaran_id = fields.Many2one('siswa_ocb11.tahunajaran', string='Tahun Ajaran', required=True)
-    jenjang = fields.Selection([(0, 'Play Group'), (1, 'TK A'), (2, 'TK B')], string='Jenjang', required=True, default=0)
+    tahunajaran_id = fields.Many2one('siswa_ocb11.tahunajaran', string='Tahun Ajaran', required=True, ondelete='cascade')
+    jenjang = fields.Selection([(0, 'PG'), (1, 'TK A'), (2, 'TK B')], string='Jenjang', required=True, default=0)
     biayas = fields.One2many('siswa_keu_ocb11.biaya_ta_jenjang', inverse_name='tahunajaran_jenjang_id' , string='Biaya-biaya')
 
-    @api.model
-    def create(self, vals):
-        vals['name'] = str(vals['tahunajaran_id']) + str(vals['jenjang'])
+    # @api.model
+    # def create(self, vals):
+    #     vals['name'] = str(vals['tahunajaran_id']) + str(vals['jenjang'])
 
-        result = super(tahunajaran_jenjang, self).create(vals)
-        return result
+    #     result = super(tahunajaran_jenjang, self).create(vals)
+    #     return result
