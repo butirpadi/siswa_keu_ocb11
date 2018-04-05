25462cc98f0d4dc6c4da04d37662c519eb5a2931
Butirpadi <butirpadi@gmail.com>
2018-04-05 16:14:23 +0700

    [UPDATE] Biaya

--- models/tahunajaran_jenjang.py
+++ models/tahunajaran_jenjang.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api, exceptions, _
 from pprint import pprint
 import calendar
 
@@ -11,82 +11,90 @@ class tahunajaran_jenjang(models.Model):
     
     def validate_state(self):
         self.ensure_one()
-        rombel_ids = self.env['siswa_ocb11.rombel'].search([('jenjang','=',int(self.jenjang))]).ids
-        siswas = self.env['siswa_ocb11.rombel_siswa'].search([
-                    ('tahunajaran_id','=',self.tahunajaran_id.id),
-                    ('rombel_id','in',rombel_ids),
-                ])
-        # create/insert biaya to siswa
-        for sis in siswas:
-            for by in self.biayas:
-                if by.biaya_id.is_bulanan:
-                    for bulan_index in range(1,13):
-                        print(bulan_index)
-                        self.env['siswa_keu_ocb11.siswa_biaya'].create({
-                            'name' : by.biaya_id.name + ' ' + calendar.month_name[bulan_index],
-                            'siswa_id' : sis.siswa_id.id,
-                            'tahunajaran_id' : self.tahunajaran_id.id,
-                            'biaya_id' : by.biaya_id.id,
-                            'bulan' : bulan_index,
-                            'harga' : by.harga,
-                            'amount_due' : by.harga,
-                        })
-                else:
-                    self.env['siswa_keu_ocb11.siswa_biaya'].create({
-                        'name' : by.biaya_id.name,
-                        'siswa_id' : sis.siswa_id.id,
-                        'tahunajaran_id' : self.tahunajaran_id.id,
-                        'biaya_id' : by.biaya_id.id,
-                        'harga' : by.harga,
-                        'amount_due' : by.harga,
-                    })
-            # set total_biaya dan amount_due
-            total_biaya = sum(by.harga for by in self.biayas)
-            self.env['res.partner'].search([('id','=',sis.siswa_id.id)]).write({
-                'total_biaya' : total_biaya,
-                'amount_due_biaya' : total_biaya,
-            })
-            # print(sis.siswa_id.name + ' ' + sis.siswa_id.active_rombel_id.name)
-        # update state
-        self.write({
-            'state' : 'valid'
-        })
-    
-    def recompute_biaya(self):
-        self.ensure_one()
-        print('Recompute Biaya Siswa')
-        siswas = self.env['siswa_ocb11.rombel_siswa'].search([('tahunajaran_id','=',self.tahunajaran_id.id),('jenjang','=',self.jenjang)])
-        for biy in self.biayas:
+        if self.biayas and len(self.biayas) > 0:
+            rombel_ids = self.env['siswa_ocb11.rombel'].search([('jenjang','=',int(self.jenjang))]).ids
+            print(rombel_ids)
+            siswas = self.env['siswa_ocb11.rombel_siswa'].search([
+                        ('tahunajaran_id','=',self.tahunajaran_id.id),
+                        ('rombel_id','in',rombel_ids),
+                    ])
+            # create/insert biaya to siswa
             for sis in siswas:
-                found = False
-                for siswa_biaya in sis.siswa_id.biayas:
-                    if(siswa_biaya.biaya_id.id == biy.biaya_id.id):
-                        found = True
-                
-                if not found:
-                    print('Add biaya to : ' + sis.siswa_id.name)
-                    # jika tidak tersedia maka inputkan baru
-                    if biy.biaya_id.is_bulanan:
+                for by in self.biayas:
+                    if by.biaya_id.is_bulanan:
                         for bulan_index in range(1,13):
                             self.env['siswa_keu_ocb11.siswa_biaya'].create({
-                                'name' : biy.biaya_id.name + ' ' + calendar.month_name[bulan_index],
+                                'name' : by.biaya_id.name + ' ' + calendar.month_name[bulan_index],
                                 'siswa_id' : sis.siswa_id.id,
                                 'tahunajaran_id' : self.tahunajaran_id.id,
-                                'biaya_id' : biy.biaya_id.id,
+                                'biaya_id' : by.biaya_id.id,
                                 'bulan' : bulan_index,
-                                'harga' : biy.harga,
+                                'harga' : by.harga,
                                 'amount_due' : by.harga,
                             })
                     else:
                         self.env['siswa_keu_ocb11.siswa_biaya'].create({
-                            'name' : biy.biaya_id.name,
+                            'name' : by.biaya_id.name,
                             'siswa_id' : sis.siswa_id.id,
                             'tahunajaran_id' : self.tahunajaran_id.id,
-                            'biaya_id' : biy.biaya_id.id,
-                            'harga' : biy.harga,
+                            'biaya_id' : by.biaya_id.id,
+                            'harga' : by.harga,
                             'amount_due' : by.harga,
                         })
-        print('End of Recompute Siswa Biaya')
+                # set total_biaya dan amount_due
+                total_biaya = sum(by.harga for by in self.biayas)
+                self.env['res.partner'].search([('id','=',sis.siswa_id.id)]).write({
+                    'total_biaya' : total_biaya,
+                    'amount_due_biaya' : total_biaya,
+                })
+                # print(sis.siswa_id.name + ' ' + sis.siswa_id.active_rombel_id.name)
+            # update state
+            self.write({
+                'state' : 'valid'
+            })
+        else:
+            # raise error warning
+            raise exceptions.except_orm(_('Warning'), _('There is no data to validate.'))        
+    
+    def recompute_biaya(self):
+        self.ensure_one()
+        print('Recompute Biaya Siswa')
+        if self.biayas and len(self.biayas) > 0:
+            siswas = self.env['siswa_ocb11.rombel_siswa'].search([('tahunajaran_id','=',self.tahunajaran_id.id),('jenjang','=',self.jenjang)])
+            for biy in self.biayas:
+                for sis in siswas:
+                    found = False
+                    for siswa_biaya in sis.siswa_id.biayas:
+                        if(siswa_biaya.biaya_id.id == biy.biaya_id.id):
+                            found = True
+                    
+                    if not found:
+                        print('Add biaya to : ' + sis.siswa_id.name)
+                        # jika tidak tersedia maka inputkan baru
+                        if biy.biaya_id.is_bulanan:
+                            for bulan_index in range(1,13):
+                                self.env['siswa_keu_ocb11.siswa_biaya'].create({
+                                    'name' : biy.biaya_id.name + ' ' + calendar.month_name[bulan_index],
+                                    'siswa_id' : sis.siswa_id.id,
+                                    'tahunajaran_id' : self.tahunajaran_id.id,
+                                    'biaya_id' : biy.biaya_id.id,
+                                    'bulan' : bulan_index,
+                                    'harga' : biy.harga,
+                                    'amount_due' : biy.harga,
+                                })
+                        else:
+                            self.env['siswa_keu_ocb11.siswa_biaya'].create({
+                                'name' : biy.biaya_id.name,
+                                'siswa_id' : sis.siswa_id.id,
+                                'tahunajaran_id' : self.tahunajaran_id.id,
+                                'biaya_id' : biy.biaya_id.id,
+                                'harga' : biy.harga,
+                                'amount_due' : biy.harga,
+                            })
+            print('End of Recompute Siswa Biaya')
+        else:
+            # raise error warning
+            raise exceptions.except_orm(_('Warning'), _('There is no data to validate.'))
 
         # print(len(siswas))
         # for slf in self:
