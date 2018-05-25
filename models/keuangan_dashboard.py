c9ed63d4753d28cec57554b7dc10a64795469842
Butirpadi <butirpadi@gmail.com>
2018-05-25 10:57:57 +0700

    Update Keuangan Dashboard

--- models/keuangan_dashboard.py
+++ models/keuangan_dashboard.py
@@ -26,40 +26,49 @@ class keuangan_dashboard(models.Model):
 
     def compute_keuangan(self):
         # get amount due
-        siswa_biayas = self.env['siswa_keu_ocb11.siswa_biaya'].search([('state','=','open')])
+        self.env.cr.execute("select sum(coalesce(amount_due,0)) from siswa_keu_ocb11_siswa_biaya where state = 'open'")
+        total_kas = self.env.cr.fetchone()[0]        
         
-        if siswa_biayas:
-            if len(siswa_biayas) > 0:
-                self.amount_due = sum(siswa_biayas.mapped('amount_due'))
+        self.amount_due = total_kas
+        
+        # # get amount due
+        # siswa_biayas = self.env['siswa_keu_ocb11.siswa_biaya'].search([('state','=','open')])
+        
+        # if siswa_biayas:
+        #     if len(siswa_biayas) > 0:
+        #         self.amount_due = sum(siswa_biayas.mapped('amount_due'))
 
-        # get amount due per tahun ajaran
-        tahunajarans = self.env['siswa_ocb11.tahunajaran'].search([
-                        ('active','ilike','%'),
-                        ('name','ilike','%')
-                    ])
-        if tahunajarans:
-            if len(tahunajarans) > 0:
-                reg_amount_due_per_ta = []
-                for ta in tahunajarans:
-                    amount_due_per_ta = sum(self.env['siswa_keu_ocb11.siswa_biaya'].search([
-                        ('state','=','open'),
-                        ('tahunajaran_id','=',ta.id),
-                        ]).mapped('amount_due'))
-                    if amount_due_per_ta > 0:
-                        reg_amount_due_per_ta.append([0,0,{
-                            'amount_due' : amount_due_per_ta,
-                            'tahunajaran_id' : ta.id
-                        }])
+        # # get amount due per tahun ajaran
+        # tahunajarans = self.env['siswa_ocb11.tahunajaran'].search([
+        #                 ('active','ilike','%'),
+        #                 ('name','ilike','%')
+        #             ])
+        # if tahunajarans:
+        #     if len(tahunajarans) > 0:
+        #         reg_amount_due_per_ta = []
+        #         for ta in tahunajarans:
+        #             amount_due_per_ta = sum(self.env['siswa_keu_ocb11.siswa_biaya'].search([
+        #                 ('state','=','open'),
+        #                 ('tahunajaran_id','=',ta.id),
+        #                 ]).mapped('amount_due'))
+        #             if amount_due_per_ta > 0:
+        #                 reg_amount_due_per_ta.append([0,0,{
+        #                     'amount_due' : amount_due_per_ta,
+        #                     'tahunajaran_id' : ta.id
+        #                 }])
                 
-                # delete data sebelumnya
-                self.amount_due_per_tahunajaran.unlink()
-                # input data terbaru
-                self.amount_due_per_tahunajaran = reg_amount_due_per_ta
+        #         # delete data sebelumnya
+        #         self.amount_due_per_tahunajaran.unlink()
+        #         # input data terbaru
+        #         self.amount_due_per_tahunajaran = reg_amount_due_per_ta
     
     def compute_kas(self):
-        exe_kas = self.env.cr.execute('select sum(debet)-sum(kredit) from siswa_keu_ocb11_kas')
+        self.env.cr.execute("select sum(coalesce(debet,0))-sum(coalesce(kredit,0)) as saldo from siswa_keu_ocb11_kas where state ='post' ")
         total_kas = self.env.cr.fetchone()[0]        
         self.total = total_kas
+        self.amount_due = total_kas
+        print('saldo kas (total) : '  + str(self.total))
+        print('saldo kas (amount_due) : '  + str(self.amount_due))
     
     def get_default_name(self):
         default_name = self.env['ir.model.data'].search([
