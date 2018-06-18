a138bcd3ea66f6040ee8716f57f9be1b81c91ce8
Butirpadi <butirpadi@gmail.com>
2018-06-18 10:10:56 +0700

    Update user access

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import biaya
-# from . import tahunajaran
-# from . import tahunajaran_biaya
 from . import tahunajaran_jenjang
 from . import biaya_ta_jenjang
 from . import siswa
