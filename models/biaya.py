c74ab0056c9db4b8e23877906854ca6cd19c0b1e
Eries Herman <butirpadi@outlook.com>
2018-07-29 16:51:25 +0700

    Revert "Add is_opsional to biaya"
    This reverts commit b74f89a730208444db896d83b369b67673350b62.

--- models/biaya.py
+++ models/biaya.py
@@ -9,7 +9,5 @@ class biaya(models.Model):
     is_bulanan = fields.Boolean('Bulanan',default=False)
     is_different_by_gender = fields.Boolean('Different by Gender?', default=False)
     is_siswa_baru_only = fields.Boolean('Hanya untuk siswa baru?', default=False)
-    is_optional = fields.Boolean('Biaya Opsional?', default=False)
-    
     # harga = fields.Float('Harga', default=0.00, required=True)
     # tahunajarans = fields.One2many('siswa_keu_ocb11.tahunajaran_biaya', inverse_name='biaya_id' , string='Tahun Ajaran')
